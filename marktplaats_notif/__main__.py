40e3d49e382a03e2473eda2e4bdc15cec3b9121d
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-01-01 17:15:13 +0100

    Add configuration schema

--- marktplaats_notif/__main__.py
+++ marktplaats_notif/__main__.py
@@ -2,9 +2,12 @@ import sys
 
 from marktplaats import SearchQuery
 
+from marktplaats_notif.config import config
+
 
 def main() -> int:
     print("Hello world!")
+    print(config["general"]["interval"])
     print(SearchQuery("fiets", limit=1).get_listings()[0].title)
     return 0
 
--- marktplaats_notif/config.py
+++ marktplaats_notif/config.py
@@ -1,9 +1,53 @@
+import tomllib
 from pathlib import Path
 from shutil import copy
+from string import digits, ascii_uppercase
+
+from schema import Schema, And, Optional
+
 
 config_dir = Path("/config")
 config_file = config_dir / "config.toml"
 
+positive_int = And(int, lambda n: n > 0)
+
+def is_zip_code(s: str) -> bool:
+    # This is intentionally strict as 1234AB is the only tested format.
+    #  Spaces, lowercase, etc. all might work, but are untested.
+    return (
+        len(s) == 6
+        and all(c in digits for c in s[:4])
+        and all(c in ascii_uppercase for c in s[4:])
+    )
+
+def is_http(s: str) -> bool:
+    return s.startswith("http://") or s.startswith("https://")
+
+search_schema = {
+    Optional("query"): And(str, len),
+    Optional("price_from"): positive_int,
+    Optional("price_to"): positive_int,
+    Optional("zip_code"): And(str, is_zip_code),
+    Optional("distance"): positive_int,
+}
+
+schema = Schema({
+    "general": {
+        "interval": positive_int,
+    },
+    "notifications": {
+        "ntfy": {
+            "endpoint": And(str, is_http),
+        }
+    },
+    "global": search_schema,
+    "searches": [search_schema],
+})
+
 if not config_file.exists():
     copy("default_config.toml", config_file)
     print("Default configuration created")
+
+
+with config_file.open("b") as file:
+    config = schema.validate(tomllib.load(file))
